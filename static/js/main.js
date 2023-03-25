410721ec9ff3c3838526d895ebb2baa8dd341163
Mawuli Stephen <mawulistephens@gmail.com>
2023-03-25 19:52:59 +0000

    update on blog site

---
+++ static/js/main.js
@@ -0,0 +1,113 @@
+const menuLinks = document.querySelectorAll(".menu-link");
+
+
+// Get Modal
+var modal = document.getElementById('myModal');
+
+//This button calls the modal when clicked
+const modalBtn = document.getElementById('myBtn');
+
+// Get the <span> element to close Modal
+var closeModal = document.getElementsByClassName("close")[0];
+
+
+
+// JavaScript statement menu
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menuLinks.forEach((link) => {
+            link.classList.remove("is-active");
+        });
+        link.classList.add("is-active");
+    });
+});
+
+
+
+// JavaScript statement for modal Menu
+
+
+// When user clicks button, open Modal
+modalBtn.onclick = function () {
+    modal.style.display = "block";
+    
+};
+
+closeModal.onclick = function () {
+    modal.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+// Contact form JS
+function addClass() {
+    document.body.classList.add("sent");
+  }
+  
+  sendLetter.addEventListener("click", addClass);
+
+
+
+
+//   categories
+
+filterSelection("all") // Execute the function and show all columns
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
